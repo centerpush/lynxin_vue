83031ede02d508bdfbdc4bac1302ed3bb16c645d
anjelicakay <aktowner@gmail.com>
2019-02-20 15:50:37 -0600

    added div classes to profile view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,23 +2,24 @@
   <div class="profile">
     <div class ="primary">
       <div class="info">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div class="name"></div>
+        <div class="profile-pic"></div>
+        <div class="location"></div>
+        <div class="title"></div>
+        <div class="owner"></div>
+        <div class="summary"></div>
       </div>
       <div class="education">
-        <div></div>
-        <div></div>
+        <div class="education-1"></div>
+        <div class="experience"></div>
+        <div class="accomplishments"></div>
       </div>
       <div class="skills">
-        <div></div>
-        <div></div>
+        <div class="skills-1"></div>
+        <div class="endorsments"></div>
       </div>
       <div class="interests">
-        <div></div>
+        <div class="intersts-1"></div>
       </div>
     </div>
     <div class="secondary">
