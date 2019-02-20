ce6d841b0d70de7f5a32bba63fdb02059d933923
anjelicakay <aktowner@gmail.com>
2019-02-20 14:40:11 -0600

    Welcome Page

---
+++ src/views/Welcome.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="welcome">
+
+    <div>
+      <form>
+      <h1>Sign Up</h1>
+        <p>Find your own definition of success with help from an inspiring professional cat community</p>
+        <div>
+        Email: <input type="text" v-model="newUserEmail">
+        </div>
+        <div>
+        Password: <input type="text" v-model="newUserPassword">
+        </div>
+        <div>
+        Confirm password: <input type="text" v-model="newUserPasswordVerification">
+        </div>
+        <input type="submit" value="Join">
+      </form>
+    </div>
+
+  </div>
+</template>
+
+<style>
+  
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      newUserEmail: "",
+      newUserPassword: "",
+      newUserPasswordVerification: ""
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
