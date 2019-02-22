22395b39bd83ad05f13caa9cfa42e707ef31c573
centerpush <dylancross100@gmail.com>
2019-02-22 15:13:57 -0600

    Made a couple bug fixes and started the view work on CatsShow

--- src/views/CatsNew.vue
+++ src/views/CatsNew.vue
@@ -1,1 +1,105 @@
-CatsNew.vue
+<template>
+  <div class="cats-new">
+    <h1>New Cat, Meow Meow</h1>
+
+    <ul>
+      <li v-for="error in errors"{{ error }}</li>
+    </ul>
+    
+    <form v-on:submit.prevent='submit()'>
+      <div>
+        <input v-model="newCatFirstName" placeholder="First Name">
+      </div>
+      <div>
+        <input v-model="newCatFamilyName" placeholder="Family Name">
+      </div>
+      <div>
+        <input v-model="newCatTitle" placeholder="Title">
+      </div>
+      <div>
+        <input v-model="newCatEducation" placeholder="Education">
+      </div>
+      <div>
+        <input v-model="newCatExperience" placeholder="Experience">
+      </div>
+      <div>
+        <input v-model="newCatLocation" placeholder="Location">
+      </div>
+      <div>
+        <input v-model="newCatOwner" placeholder="Owner">
+      </div>
+      <div>
+        <input v-model="newCatSummary" placeholder="Summary">
+      </div>
+      <div>
+        <input v-model="newCatProfileUlr" placeholder="Profile Url">
+      </div>
+      <div>
+        <input v-model="newCatAccomplishments" placeholder="Accomplishments">
+      </div>
+      <div>
+        <input v-model="newCatEndorsements" placeholder="Endorsements">
+      </div>
+      <div>
+        <input v-model="newCatSkills" placeholder="Skills">
+      </div>
+      
+
+      <input type="submit" value="Create" class="btn btn-primary">
+    </form>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+      first_name: "",
+      family_name: "",
+      title: "",
+      education: "",
+      experience: "",
+      location: "",
+      owner: "",
+      summary: "",
+      profile_url: "",
+      accomplishments: "",
+      endorsements: "",
+      skills: "",
+      errors: []
+    };
+  },
+  created: function() {},
+  methods: {
+    submit: function() {
+      console.log("Creating Cat...");
+      var params = {
+                    first_name: this.newCatfirst_name,
+                    family_name: this.newCatfamily_name,
+                    title: this.newCattitle,
+                    education: this.newCateducation,
+                    experience: this.newCatexperience,
+                    location: this.newCatlocation,
+                    owner: this.newCatowner,
+                    summary: this.newCatsummary,
+                    profile_url: this.newCatprofile_url,
+                    accomplishments: this.newCataccomplishments,
+                    endorsements: this.newCatendorsements,
+                    skills: this.newCatskills
+                    };
+      axios.post("/api/cats", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.$router.push("/");
+        }).catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    }
+  }
+};
+</script>
--- src/views/CatsShow.vue
+++ src/views/CatsShow.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="cats-show">
-    <h2> {{ cats.title  }} </h2>
+    <div class="container">
+      <div class="card" style="width: 60rem;">
+        <!-- <img :src='cat.profile_url' class="card-img-top" alt="Not loading"> -->
+        <div class="card-body">
+          <h5 class="card-title">{{ cat.first_name  }} {{  cat.family_name}}</h5>
+          <p class="card-text">{{ cat.title }}</p>
+          <p class="card-text">{{ cat.location }}</p>
+          <p class="card-text">{{ cat.ownder }}</p>
+          <p class="card-text">{{ cat.summary }}</p>
+          <a href="#" class="btn btn-primary">Connect</a>
+        </div>
+
+        <div class="card" style="width: 60rem;">
+          <div class="card-body">
+            <p class="card-text">{{ cat.experience }}</p>
+            <p class="card-text">{{ cat.education }}</p>
+          </div>
+        </div>
+
+        <div class="card" style="width: 60rem;">
+          <div class="card-body">
+            <p class="card-text">{{ cat.skills }}</p>
+            <p class="card-text">{{ cat.endorsements }}</p>
+            <p class="card-text">{{ cat.accomplishments }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -14,7 +42,7 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      cats: {
+      cat: {
         first_name: "",
         family_name: "",
         title: "",
@@ -34,7 +62,7 @@ export default {
   created: function() {
     axios.get("/api/cats/" + this.$route.params.id)
     .then(response => {
-      this.cats = response.data;
+      this.cat = response.data;
     });
   },
   methods: {}
